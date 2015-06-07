75fe9f9603bb8973e2aad5ecfa2cb63b3b76ea34
Dimitri 'phaazon' Sabadie <dimitri.sabadie@gmail.com>
2015-06-07 21:54:16 +0200

    FreeType quantified imports.
    We now quantifically import FreeType modules to be able to use
    very important symbols, such as width, height, rows and so on.

--- src/Main.hs
+++ src/Main.hs
@@ -20,19 +20,19 @@ import Data.Traversable
 import Data.Vector.Storable ( fromList )
 import Foreign
 import Foreign.C.String
-import Graphics.Rendering.FreeType.Internal
-import Graphics.Rendering.FreeType.Internal.Bitmap
-import Graphics.Rendering.FreeType.Internal.Face
-import Graphics.Rendering.FreeType.Internal.GlyphSlot
-import Graphics.Rendering.FreeType.Internal.PrimitiveTypes
+import qualified Graphics.Rendering.FreeType.Internal as FT
+import qualified Graphics.Rendering.FreeType.Internal.Bitmap as FT
+import qualified Graphics.Rendering.FreeType.Internal.Face as FT
+import qualified Graphics.Rendering.FreeType.Internal.GlyphSlot as FT
+import qualified Graphics.Rendering.FreeType.Internal.PrimitiveTypes as FT
 import System.Environment
 
-liftErr :: (MonadError String m) => FT_Error -> m ()
+liftErr :: (MonadError String m) => FT.FT_Error -> m ()
 liftErr e
   | e == 0 = pure ()
   | otherwise = throwError $ "FreeType error: " ++ show e
 
-wrapErr :: (MonadIO m,MonadError String m) => IO FT_Error -> m ()
+wrapErr :: (MonadIO m,MonadError String m) => IO FT.FT_Error -> m ()
 wrapErr a = liftIO a >>= liftErr
 
 main :: IO ()
@@ -44,32 +44,32 @@ main = do
       padding = read paddingStr
   r <- alloca $ \ftlibPtr ->
     alloca $ \facePtr -> runEitherT $ do
-      wrapErr $ ft_Init_FreeType ftlibPtr
+      wrapErr $ FT.ft_Init_FreeType ftlibPtr
       ftlib <- liftIO $ peek ftlibPtr
       wrapErr . withCString ttfPath $ \cttfPath ->
-        ft_New_Face ftlib cttfPath 0 facePtr
-      fc <- liftIO $ peek facePtr
-      wrapErr $ ft_Set_Char_Size fc 0 (pt*64) dpiX dpiY
+        FT.ft_New_Face ftlib cttfPath 0 facePtr
+      face <- liftIO $ peek facePtr
+      wrapErr $ FT.ft_Set_Char_Size face 0 (pt*64) dpiX dpiY
       alphabet <- liftIO $ fmap lines getContents
       let alphabetSize = length alphabet
       bitmaps <- for alphabet $ \line -> do
         for line $ \c -> do
           liftIO . putStrLn $ "processing char: " ++ show c
-          wrapErr $ ft_Load_Char fc (fromIntegral $ fromEnum c) (ft_LOAD_RENDER .|. ft_LOAD_MONOCHROME)
-          glph <- liftIO . peek $ glyph fc
-          btmp <- liftIO . peek $ bitmap glph
-          unless (pixel_mode btmp == 1) . throwError $ show c ++ " is not encoded as a monochrome bitmap"
-          pixels <- liftIO $ extractBitmap btmp
+          wrapErr $ FT.ft_Load_Char face (fromIntegral $ fromEnum c) (FT.ft_LOAD_RENDER .|. FT.ft_LOAD_MONOCHROME)
+          glyph <- liftIO . peek $ FT.glyph face
+          bitmap <- liftIO . peek $ FT.bitmap glyph
+          unless (FT.pixel_mode bitmap == 1) . throwError $ show c ++ " is not encoded as a monochrome bitmap"
+          pixels <- liftIO $ extractBitmap bitmap
           liftIO $ do
-            top <- fmap fromIntegral $ peek $ bitmap_top glph
-            left <- peek $ bitmap_left glph
-            let rws = fromIntegral $ rows btmp
-                w = fromIntegral $ width btmp
-            putStrLn $ "rows: " ++ show rws
-            putStrLn $ "width: " ++ show w
+            top <- fmap fromIntegral $ peek $ FT.bitmap_top glyph
+            left <- peek $ FT.bitmap_left glyph
+            let rows = fromIntegral $ FT.rows bitmap
+                width = fromIntegral $ FT.width bitmap
+            putStrLn $ "rows: " ++ show rows
+            putStrLn $ "width: " ++ show width
             putStrLn $ "top: " ++ show top
             putStrLn $ "left: " ++ show left
-            pure (rws,w, pixels)
+            pure (rows,width, pixels)
       let (maxRow,_,maxG) = maximumBy (\(a,_,_) (b,_,_) -> compare a b) (concat bitmaps)
           (_,maxWidth,maxGW) = maximumBy (\(_,a,_) (_,b,_) -> compare a b) (concat bitmaps)
           bitmaps' = map (mergeBitmapLine (maxRow + padding) . map (resize (maxRow + padding) (maxWidth + padding))) bitmaps
@@ -91,23 +91,23 @@ unpackPixels p = map (\x -> if x == 0 then 0 else 255)
   , p .&. 1
   ]
 
-extractBitmap :: FT_Bitmap -> IO [[Word8]]
-extractBitmap btmp = go 0 (buffer btmp)
+extractBitmap :: FT.FT_Bitmap -> IO [[Word8]]
+extractBitmap bitmap = go 0 (FT.buffer bitmap)
   where
-    rws = rows btmp
-    w = width btmp
+    rows = FT.rows bitmap
+    width = FT.width bitmap 
     go col buf
-      | col < rws = do
-          line <- fmap (take (fromIntegral w) . concatMap unpackPixels) $ peekArray (ceiling $ fromIntegral w / 8) (castPtr buf)
-          nextLines <- go (succ col) (buf `advancePtr` fromIntegral (pitch btmp))
+      | col < rows = do
+          line <- fmap (take (fromIntegral width) . concatMap unpackPixels) $ peekArray (ceiling $ fromIntegral width / 8) (castPtr buf)
+          nextLines <- go (succ col) (buf `advancePtr` fromIntegral (FT.pitch bitmap))
           pure $ line : nextLines
       | otherwise = pure []
 
 resize :: Int -> Int -> (Int,Int,[[Word8]]) -> [[Word8]]
-resize maxRow maxWidth (rws,w,pixels) = pixels' ++ replicate (maxRow - rws) (replicate maxWidth 0)
+resize maxRow maxWidth (rows,width,pixels) = pixels' ++ replicate (maxRow - rows) (replicate maxWidth 0)
   where
     pixels' = map (++ pad) pixels
-    pad = replicate (maxWidth - w) 0
+    pad = replicate (maxWidth - width) 0
 
 -- FIXME
 -- Merge a line of bitmaps as one.
@@ -119,4 +119,4 @@ mergeBitmapLine remainingRows line
 
 -- Build an image out of the rendered glyphs
 turnToFontmap :: Int -> Int -> [[[[Word8]]]] -> JP.Image JP.Pixel8
-turnToFontmap rowNb w glyphs = JP.Image w rowNb (fromList . concat . concat $ concat glyphs)
+turnToFontmap rows width glyphs = JP.Image width rows (fromList . concat . concat $ concat glyphs)
