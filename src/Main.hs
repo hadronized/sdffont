5f988d41671fdcc4c648a2752032d78db7017ad6
Dimitri 'phaazon' Sabadie <dimitri.sabadie@gmail.com>
2015-06-09 23:54:46 +0200

    Refactoring completed.

--- src/Main.hs
+++ src/Main.hs
@@ -16,9 +16,11 @@ import Control.Monad.Trans
 import Control.Monad.Trans.Either
 import Data.Bits
 import Data.Foldable
+import Data.Monoid
 import Data.Traversable
-import Data.Vector as V ( Vector, fromList )
--- import Data.Vector.Storable as SV ( Vector, fromList )
+import Data.Vector as V ( Vector, (!), cons, convert, fromList, singleton )
+import qualified Data.Vector as V ( concat, concatMap, head, replicate, tail )
+import qualified Data.Vector.Storable as SV ( Vector, concat )
 import Foreign
 import Foreign.C.String
 import qualified Graphics.Rendering.FreeType.Internal as FT
@@ -32,11 +34,15 @@ import System.Environment
 -- prefered though for O(1) on random access.
 type Pixel = Word8
 
--- A glyph is represented as several pixel lines.
-type Glyph = Vector (Vector Pixel)
+-- A bitmap.
+type Bitmap = Vector (Vector Pixel)
+
+-- A glyph is represented as several pixel lines along with information about
+-- size and offset (width,rows,top,left,pixels).
+type Glyph = (Int,Int,Int,Int,Bitmap)
 
 -- A fontmap is encoded as several glyph lines.
-type Fontmap = Vector (Vector Glyph)
+type Fontmap = Vector (Vector Bitmap)
 
 -- Get the alphabet from MonadIO m.
 --
@@ -85,6 +91,7 @@ processFont :: FilePath
             -> Int
             -> IO (Either String ())
 processFont fontPath alphabet output pt dpix dpiy padding = do
+  traverse_ putStrLn alphabet
   alloca $ \ftlibPtr ->
     alloca $ \facePtr -> runEitherT $ do
       wrapErr $ FT.ft_Init_FreeType ftlibPtr
@@ -94,15 +101,24 @@ processFont fontPath alphabet output pt dpix dpiy padding = do
       face <- liftIO $ peek facePtr
       wrapErr $ FT.ft_Set_Char_Size face 0 (fromIntegral pt * 64)
         (fromIntegral dpix) (fromIntegral dpiy)
-      pure ()
+      fontmap <- traverse (traverse $ createGlyph face) alphabet'
+      let bitmaps = fmap (mergeBitmapLine maxRows . fmap (\(w,h,_,_,p) -> resizeBitmap w h maxWidth maxRows p)) fontmap
+          fontmap' = V.concat (toList fontmap)
+          (maxWidth,_,_,_,_) = maximumBy (\(a,_,_,_,_) (b,_,_,_,_) -> compare a b) fontmap'
+          (_,maxRows,_,_,_) = maximumBy (\(_,a,_,_,_) (_,b,_,_,_) -> compare a b) fontmap'
+      liftIO . putStrLn $ "max width: " ++ show maxWidth
+      liftIO . putStrLn $ "max height: " ++ show maxRows
+      liftIO . JP.savePngImage output . JP.ImageY8 $ fontmapToImage
+        (maxWidth * length (head alphabet)) (maxRows * length alphabet) bitmaps
+  where
+    alphabet' = fromList $ (map fromList) alphabet
 
--- Create a new glyph by rendering it via FreeType, and by applying padding.
+-- Create a new glyph by rendering it via FreeType.
 createGlyph :: (MonadIO m,MonadError String m)
             => FT.FT_Face
-            -> Int
             -> Char
             -> m Glyph
-createGlyph face padding c = do
+createGlyph face c = do
   liftIO . putStrLn $ "processing character " ++ show c
   wrapErr $ FT.ft_Load_Char face (fromIntegral $ fromEnum c)
     (FT.ft_LOAD_RENDER .|. FT.ft_LOAD_MONOCHROME)
@@ -111,18 +127,23 @@ createGlyph face padding c = do
   unless (FT.pixel_mode bitmap == 1) . throwError $ show c ++
     " is not encoded as a monochrome bitmap"
   liftIO $ do
-    pixels <- extractGlyph bitmap
-    top <- peek $ FT.bitmap_top glyph
-    left <- peek $ FT.bitmap_left glyph
+    pixels <- extractBitmap bitmap
+    top <- fmap fromIntegral . peek $ FT.bitmap_top glyph
+    left <- fmap fromIntegral . peek $ FT.bitmap_left glyph
     let rows = fromIntegral $ FT.rows bitmap
         width = fromIntegral $ FT.width bitmap
     putStrLn $ "  width: " ++ show width
     putStrLn $ "  rows: " ++ show rows
     putStrLn $ "  top: " ++ show top
     putStrLn $ "  left: " ++ show left
-    -- TODO: correctly add padding here
-    pure pixels
+    pure (width,rows,top,left,pixels)
 
+-- Extract the bitmap out of a 'Glyph'.
+glyphBitmap :: Glyph -> Bitmap
+glyphBitmap (_,_,_,_,bitmap) = bitmap
+
+-- Get the minimum and maximum in a foldable in a single pass using an ordering
+-- function.
 minmaxBy :: (Bounded a,Foldable f)
          => (a -> a -> Ordering)
          -> f a
@@ -136,38 +157,14 @@ minmaxBy f = foldl' (\(!n,!m) a -> (min' a n,max' m a)) (maxBound,minBound)
       GT -> a
       _  -> b
 
--- Get the minimum and maximum value in a foldable in a single pass.
+-- Get the minimum and maximum in a foldable in a single pass.
 minmax :: (Bounded a,Ord a,Foldable f) => f a -> (a,a)
 minmax = minmaxBy compare
 
-{-
-main :: IO ()
-main = do
-  [ttfPath,ptStr,dpiXStr,dpiYStr,paddingStr,output] <- getArgs
-  let pt = read ptStr
-      dpiX = read dpiXStr
-      dpiY = read dpiYStr
-      padding = read paddingStr
-  r <- alloca $ \ftlibPtr ->
-    alloca $ \facePtr -> runEitherT $ do
-      wrapErr $ FT.ft_Init_FreeType ftlibPtr
-      ftlib <- liftIO $ peek ftlibPtr
-      wrapErr . withCString ttfPath $ \cttfPath ->
-        FT.ft_New_Face ftlib cttfPath 0 facePtr
-      face <- liftIO $ peek facePtr
-      wrapErr $ FT.ft_Set_Char_Size face 0 (pt*64) dpiX dpiY
-      alphabet <- fmap padAlphabet getAlphabet
-      let (maxRow,_,maxG) = maximumBy (\(a,_,_) (b,_,_) -> compare a b) (concat bitmaps)
-          (_,maxWidth,maxGW) = maximumBy (\(_,a,_) (_,b,_) -> compare a b) (concat bitmaps)
-          bitmaps' = map (mergeBitmapLine (maxRow + padding) . map (resize (maxRow + padding) (maxWidth + padding))) bitmaps
-      liftIO $ JP.savePngImage output (JP.ImageY8 $ turnToFontmap (length alphabet * (maxRow + padding)) (length (alphabet !! 0) * (maxWidth + padding)) bitmaps')
-  case r of
-    Left err -> putStrLn err
-    Right () -> putStrLn "a plus dans l'bus"
--}
-
-main = return ()
-
+-- Unpack pixels out of a byte. That function is required because FreeType
+-- packs pixels in bytes. A byte then contains 8 pixels. The generated pixels
+-- in an unnormalized monochrome color space (0-255) and are either 0 (white) or
+-- black (255).
 unpackPixels :: Word8 -> [Word8]
 unpackPixels p = map (\x -> if x == 0 then 0 else 255)
   [
@@ -181,8 +178,9 @@ unpackPixels p = map (\x -> if x == 0 then 0 else 255)
   , p .&. 1
   ]
 
-extractGlyph :: FT.FT_Bitmap -> IO Glyph
-extractGlyph bitmap = fmap fromList $ go 0 (FT.buffer bitmap)
+-- Given a FreeType glyph bitmap, that function extracts a Bitmap.
+extractBitmap :: FT.FT_Bitmap -> IO Bitmap
+extractBitmap bitmap = fmap fromList $ go 0 (FT.buffer bitmap)
   where
     rows = FT.rows bitmap
     width = FT.width bitmap
@@ -193,22 +191,38 @@ extractGlyph bitmap = fmap fromList $ go 0 (FT.buffer bitmap)
           pure $ line : nextLines
       | otherwise = pure []
 
-resize :: Int -> Int -> (Int,Int,[[Word8]]) -> [[Word8]]
-resize maxRow maxWidth (rows,width,pixels) = pixels' ++ replicate (maxRow - rows) (replicate maxWidth 0)
+-- 'resizeBitmap maxWidth maxRow bitmap' resizes 'bitmap' by resizing it
+-- regarding the 'maxWidth' and 'maxRows' arguments.
+resizeBitmap :: Int -> Int -> Int -> Int -> Bitmap -> Bitmap
+resizeBitmap width rows maxWidth maxRow bitmap = pixels' <> V.replicate (maxRow - rows) (V.replicate maxWidth 0)
   where
-    pixels' = map (++ pad) pixels
-    pad = replicate (maxWidth - width) 0
+    pixels' = fmap (<> pad) bitmap
+    pad = V.replicate (maxWidth - width) 0
 
--- FIXME
--- Merge a line of bitmaps as one.
-mergeBitmapLine :: Int -> [[[Word8]]] -> [[[Word8]]]
-mergeBitmapLine _ [] = []
+-- TODO: get rid of the singleton
+-- Merge a line of Bitmaps.
+mergeBitmapLine :: Int -> Vector Bitmap -> Vector Bitmap 
 mergeBitmapLine remainingRows line
-  | remainingRows > 0 = [concatMap head line] : mergeBitmapLine (pred remainingRows) (map tail line)
-  | otherwise = []
+  | line == mempty = mempty
+  | remainingRows > 0 = fmap V.head line `cons` mergeBitmapLine (pred remainingRows) (fmap V.tail line)
+  | otherwise = mempty
 
--- Build an image out of the rendered glyphs
-{-
-turnToFontmap :: Int -> Int -> [[[[Word8]]]] -> JP.Image JP.Pixel8
-turnToFontmap rows width glyphs = JP.Image width rows (fromList . concat . concat $ concat glyphs)
--}
+-- Build an image out of the rendered glyphs. That function expects the width
+-- and rows for a given bitmap
+fontmapToImage :: Int -> Int -> Fontmap -> JP.Image JP.Pixel8
+fontmapToImage width rows fontmap = JP.Image width rows (convert . V.concat . toList . V.concat . toList . V.concat $ toList fontmap)
+
+main :: IO ()
+main = do
+  [fontPath,ptStr,dpiXStr,dpiYStr,paddingStr,output] <- getArgs
+  let pt = read ptStr
+      dpix = read dpiXStr
+      dpiy = read dpiYStr
+      padding = read paddingStr
+  alphabet <- fmap padAlphabet getAlphabet
+  putStrLn "alphabet is:"
+  traverse_ (putStrLn . ("  "++)) alphabet
+  r <- processFont fontPath alphabet output pt dpix dpiy padding
+  case r of
+    Left err -> putStrLn err
+    Right () -> putStrLn "a plus dans l'bus"
